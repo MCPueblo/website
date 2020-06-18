5e86896739d6e0bae4525d02c21945e8e476d0fb
JuanDelPueblo <mcpuebloserver@gmail.com>
2020-06-18 15:55:48 -0400

    Large redesign

--- assets/js/slick-init.js
+++ assets/js/slick-init.js
@@ -1,7 +1,28 @@
 $(document).ready(function(){
     $('.text-slider').slick({
       arrows: true,
-      centerMode: true,
       dots: true,
+      slidesToShow: 3,
+      infinite: false,
+      responsive: [
+        {
+          breakpoint: 840,
+          settings: {
+            slidesToShow: 2,
+          }
+        },
+        {
+          breakpoint: 736,
+          settings: {
+            slidesToShow: 2,
+          }
+        },
+        {
+          breakpoint: 840,
+          settings: {
+            arrows: false,
+          }
+        }
+      ]
     });
   });
